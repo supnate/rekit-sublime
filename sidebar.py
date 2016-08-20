c190f23a298442fec175cd8575cb1bc3d0693a12
Nate Wang <supnate@gmail.com>
2016-08-20 12:18:38 +0800

    Use async thread for node execution.

--- sidebar.py
+++ sidebar.py
@@ -1,9 +1,62 @@
 #!/usr/bin/python
-import os, sublime, sublime_plugin, logging, functools, re, subprocess
+import os, sublime, sublime_plugin, logging, functools, re, subprocess, threading
 
-NODE_PATH = '/Users/i305656/.nvm/versions/node/v4.2.1/bin/node'
 def Window():
   return sublime.active_window()
+
+def main_thread(callback, *args, **kwargs):
+  # sublime.set_timeout gets used to send things onto the main thread
+  # most sublime.[something] calls need to be on the main thread
+  sublime.set_timeout(functools.partial(callback, *args, **kwargs), 0)
+
+class CommandThread(threading.Thread):
+  def __init__(self, command, on_done, working_dir="", shell="", env={}):
+    threading.Thread.__init__(self)
+    self.command = command
+    self.on_done = on_done
+    self.working_dir = working_dir
+    self.shell = shell
+    self.env = os.environ.copy()
+    self.env.update(env)
+
+  def run(self):
+    try:
+      p = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+      for line in p.stdout.readlines():
+        print(line)
+
+    except subprocess.CalledProcessError as e:
+      sublime.error_message(str(e))
+
+    except OSError as e:
+      if e.errno == 2:
+        main_thread(sublime.error_message, "Node binary could not be found in PATH\nConsider using the node_command setting for the Rekit plugin\n\nPATH is: %s" % os.environ['PATH'])
+      else:
+        raise e
+
+def run_command(command, callback=None, show_status=True, filter_empty_args=True, **kwargs):
+  if filter_empty_args:
+    command = [arg for arg in command if arg]
+
+  s = sublime.load_settings("Rekit.sublime-settings")
+  print('settings')
+  print(s.get('node_command'))
+
+  if command[0] == 'node' and s.get('node_command'):
+    command[0] = s.get('node_command')
+
+  if command[0] == 'node' and s.get('node_path'):
+    kwargs['env'] = { "NODE_PATH" : str(s.get('node_path')) }
+
+  if command[0] == 'npm' and s.get('npm_command'):
+    command[0] = s.get('npm_command')
+
+  thread = CommandThread(command, callback, **kwargs)
+  thread.start()
+
+def run_script(path, name, args = []):
+  js_file = os.path.join(get_rekit_root(path), 'tools', name + '.js')
+  run_command(['node', js_file] + args)
 
 def is_rekit_root(path):
   if path is None:
@@ -54,7 +107,6 @@ def is_component(path):
   if not os.path.exists(path):
     return False
 
-  logging.error(get_filename_without_ext(filename))
   text = open(path, 'r').read()
   if re.search('class ' + get_filename_without_ext(filename) + ' extends', text, re.MULTILINE) is None:
     return False
@@ -79,7 +131,6 @@ def is_page(path):
   if not os.path.exists(path):
     return False
 
-  logging.error(get_filename_without_ext(filename))
   text = open(path, 'r').read()
   if re.search('class ' + get_filename_without_ext(filename) + ' extends', text, re.MULTILINE) is None:
     return False
@@ -97,14 +148,6 @@ def is_actions(path):
 
 def is_other():
   return True
-
-def run_node(js, args = []):
-  p = subprocess.Popen([NODE_PATH, js] + args, stdout=subprocess.PIPE, cwd='/Users/i305656/workspace/app1', stderr=subprocess.STDOUT)
-  for line in p.stdout.readlines():
-    print(line)
-
-def run_script(path, name, args = []):
-  run_node(os.path.join(get_rekit_root(path), 'tools', name + '.js'), args)
 
 class AddFeatureCommand(sublime_plugin.ApplicationCommand):
   def run(self, paths = []):
